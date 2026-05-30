712004388ad3d763dc745891e567ed1a36a57c1f
Ilker Topcuoglu <itopcuog@uwyo.edu>
2026-05-29 22:56:09 -0600

    ABL precursor input file

---
+++ abl_precursor/run-kynema.sh
@@ -0,0 +1,22 @@
+#PBS -A wyom0218
+#PBS -N abl_spinup
+#PBS -q main
+##PBS -q debug
+#PBS -l walltime=12:00:00
+#PBS -l select=1:ncpus=20:mpiprocs=20:ompthreads=1
+#PBS -M itopcuog@uwyo.edu
+#PBS -m bea
+
+# Load modules to match compile-time environment
+module purge
+module load ncarenv/24.12  intel-oneapi/2024.2.1 cray-mpich/8.1.29
+
+export KYNEMA_DIR=/glade/campaign/uwyo/wyom0218/topcuoglu/kynema-manager/
+cd ${KYNEMA_DIR}
+source shortcut.sh
+quick-activate environments/kynema_hypre
+spack load kynema-driver
+cd -
+
+
+mpiexec --cpu-bind depth -n 20 -ppn 20 -d 1 kynema_sgf  ./abl_precursor.inp &> abl_precursor.log

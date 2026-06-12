15f65e523c68eca41b26acb27c39805ee3e4dce8
Ilker Topcuoglu <itopcuog@uwyo.edu>
2026-06-12 10:53:51 -0600

    Restarted ABL with inflow

---
+++ abl_inflow/run-kynema.sh
@@ -0,0 +1,22 @@
+#PBS -A wyom0218
+#PBS -N abl_inflow
+#PBS -q main
+##PBS -q debug
+#PBS -l walltime=01:00:00
+#PBS -l select=1:ncpus=128:mpiprocs=20:ompthreads=1
+#PBS -M itopcuog@uwyo.edu
+#PBS -m bea
+
+# Load modules to match compile-time environment
+module purge
+module load ncarenv/24.12  intel-oneapi/2024.2.1 cray-mpich/8.1.29
+
+export KYNEMA_DIR=/glade/campaign/uwyo/wyom0218/topcuoglu/kynema-manager/
+cd ${KYNEMA_DIR}
+source shortcut.sh
+quick-activate environments/kynema_hypre
+spack load kynema-driver
+cd -
+
+
+mpiexec --cpu-bind depth -n 128 -ppn 128 -d 1 kynema_sgf  ./abl_inflow.inp &> abl_inflow.log
